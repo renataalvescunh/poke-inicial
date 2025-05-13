6a54ac6f8df19bd6e95574b0ba8c83a21b8560be
Renata <renataalvescunh@gmail.com>
2025-05-13 17:20:34 -0300

    tirando '/'

--- js/imagens.js
+++ js/imagens.js
@@ -2,53 +2,53 @@
 
 const imagensPorRegiao = {
     kanto: [
-        '/assets/imgs/001.png', // Bulbasaur
-        '/assets/imgs/004.png', // Charmander
-        '/assets/imgs/007.png' // Squirtle
+        'assets/imgs/001.png', // Bulbasaur
+        'assets/imgs/004.png', // Charmander
+        'assets/imgs/007.png' // Squirtle
     ],
     johto: [
-        '/assets/imgs/152.png',
-        '/assets/imgs/155.png',
-        '/assets/imgs/158.png'
+        'assets/imgs/152.png',
+        'assets/imgs/155.png',
+        'assets/imgs/158.png'
     ],
     hoenn: [
-        '/assets/imgs/252.png',
-        '/assets/imgs/255.png',
-        '/assets/imgs/258.png'
+        'assets/imgs/252.png',
+        'assets/imgs/255.png',
+        'assets/imgs/258.png'
     ],
     sinnoh: [
-        '/assets/imgs/387.png',
-        '/assets/imgs/390.png',
-        '/assets/imgs/393.png'
+        'assets/imgs/387.png',
+        'assets/imgs/390.png',
+        'assets/imgs/393.png'
     ],
     unova: [
-        '/assets/imgs/495.png',
-        '/assets/imgs/498.png',
-        '/assets/imgs/501.png'
+        'assets/imgs/495.png',
+        'assets/imgs/498.png',
+        'assets/imgs/501.png'
     ],
     kalos: [
-        '/assets/imgs/650.png',
-        '/assets/imgs/653.png',
-        '/assets/imgs/656.png'
+        'assets/imgs/650.png',
+        'assets/imgs/653.png',
+        'assets/imgs/656.png'
     ],
     alola: [
-        '/assets/imgs/722.png',
-        '/assets/imgs/725.png',
-        '/assets/imgs/728.png'
+        'assets/imgs/722.png',
+        'assets/imgs/725.png',
+        'assets/imgs/728.png'
     ],
     galar: [
-        '/assets/imgs/810.png',
-        '/assets/imgs/813.png',
-        '/assets/imgs/816.png'
+        'assets/imgs/810.png',
+        'assets/imgs/813.png',
+        'assets/imgs/816.png'
     ],
     hisui: [
-        '/assets/imgs/722-hisui.png',
-        '/assets/imgs/155-hisui.png',
-        '/assets/imgs/501-hisui.png'
+        'assets/imgs/722-hisui.png',
+        'assets/imgs/155-hisui.png',
+        'assets/imgs/501-hisui.png'
     ],
     paldea: [
-        '/assets/imgs/906.png',
-        '/assets/imgs/909.png',
-        '/assets/imgs/912.png'
+        'assets/imgs/906.png',
+        'assets/imgs/909.png',
+        'assets/imgs/912.png'
     ]
 };
